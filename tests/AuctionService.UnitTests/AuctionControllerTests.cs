372ede76b888b73c09f345cebdc75bbaf80be0c0
HLockward <HLockward12@gmail.com>
2025-03-18 01:29:53 -0400

    Added AuctionService.UnitTest project to the solution and created the AuctionControllerTests with the Get end-point tests

---
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -0,0 +1,80 @@
+using AuctionService.Controllers;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionController _controller;
+    private readonly IMapper _mapper;
+    public AuctionControllerTests()
+    {
+        _fixture = new Fixture();
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint = new Mock<IPublishEndpoint>();
+
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+
+        _mapper = new Mapper(mapperConfig);
+        _controller = new AuctionController(_auctionRepo.Object,
+            _mapper, _publishEndpoint.Object);
+    }
+
+    [Fact]
+    public async Task GetAuctions_WithNoParams_ReturnsAuctionDtoListOf10()
+    {
+        // Arrange
+        var auctions = _fixture.CreateMany<AuctionDto>(10).ToList();
+        _auctionRepo.Setup(x => x.GetAuctionsAsync(null)).ReturnsAsync(auctions);
+
+        // Act
+        var result = await _controller.GetAllAuction(null);
+
+        // Assert
+        Assert.Equal(10, result.Value.Count);
+        Assert.IsType<ActionResult<List<AuctionDto>>>(result);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ReturnsAuctionDto()
+    {
+        // Arrange
+        var auction = _fixture.Create<AuctionDto>();
+        _auctionRepo.Setup(x => x.GetAuctionByIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(auction);
+
+        // Act
+        var result = await _controller.GetAuctionById(auction.Id);
+
+        // Assert
+        Assert.Equal(auction, result.Value);
+        Assert.IsType<ActionResult<AuctionDto>>(result);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidId_ReturnsNotFound()
+    {
+        // Arrange
+        _auctionRepo.Setup(x => x.GetAuctionByIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(value: null);
+
+        // Act
+        var result = await _controller.GetAuctionById(Guid.NewGuid());
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+}
