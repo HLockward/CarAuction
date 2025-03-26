5a794409715286c45848ba4b968361d147d5b359
HLockward <HLockward12@gmail.com>
2025-03-25 21:42:33 -0400

    Added the page filter to the listing

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,10 +2,11 @@
 import { Auction, PageResult } from "../types";
 
 export async function getData(
-  pageNumber: number = 1
+  pageNumber: number,
+  pageSize: number
 ): Promise<PageResult<Auction>> {
   const response = await fetch(
-    `http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`
+    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
   );
   if (!response.ok) throw new Error("Failed to fetch data");
   return response.json();
--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -4,23 +4,26 @@ import { getData } from "../actions/auctionActions";
 import AppPagination from "../components/AppPagination";
 import { Auction } from "../types";
 import AuctionCard from "./AuctionCard";
+import Filters from "./Filters";
 
 const Listing = () => {
   const [auctions, setAuctions] = useState<Auction[]>([]);
   const [pageCount, setPageCount] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(4);
 
   useEffect(() => {
-    getData(pageNumber).then((data) => {
+    getData(pageNumber, pageSize).then((data) => {
       setAuctions(data.result);
       setPageCount(data.pageCount);
     });
-  }, [pageNumber]);
+  }, [pageNumber, pageSize]);
 
   if (auctions.length === 0) return <div>Loading...</div>;
 
   return (
     <>
+      <Filters pageSize={pageSize} setPageSize={setPageSize} />
       <div className="grid grid-cols-4 gap-6">
         {auctions.map((auction) => (
           <AuctionCard key={auction.id} auction={auction} />
