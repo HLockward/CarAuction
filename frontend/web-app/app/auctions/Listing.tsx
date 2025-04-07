e32834ffc198bc60ac931badf624c24c56b236d3
HLockward <HLockward12@gmail.com>
2025-04-06 23:45:24 -0400

    Added the functionality to search my auctions and the auctions won

--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -19,6 +19,8 @@ const Listing = () => {
       searchTerm: state.searchTerm,
       orderBy: state.orderBy,
       FilterBy: state.filterBy,
+      seller: state.seller,
+      winner: state.winner,
     }))
   );
   const setParams = useParamsStore((state) => state.setParams);
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { useParamsStore } from "@/hooks/useParamsStore";
 import { Dropdown, DropdownDivider, DropdownItem } from "flowbite-react";
 import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
 import { HiUser } from "react-icons/hi";
 import { HiCog } from "react-icons/hi2";
@@ -14,13 +15,26 @@ type Props = {
 
 const UserActions = ({ user }: Props) => {
   const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore((state) => state.setParams);
+
+  const setWinner = () => {
+    setParams({ winner: user.username, seller: undefined });
+    if (pathname !== "/") router.push("/");
+  };
+
+  const setSeller = () => {
+    setParams({ seller: user.username, winner: undefined });
+    if (pathname !== "/") router.push("/");
+  };
+
   return (
     <Dropdown inline label={`welcome ${user.name}`}>
-      <DropdownItem icon={HiUser}>
-        <Link href="/">My Auctions</Link>
+      <DropdownItem icon={HiUser} onClick={setSeller}>
+        My Auctions
       </DropdownItem>
-      <DropdownItem icon={AiFillTrophy}>
-        <Link href="/">Auctions won</Link>
+      <DropdownItem icon={AiFillTrophy} onClick={setWinner}>
+        Auctions won
       </DropdownItem>
       <DropdownItem icon={AiFillCar}>
         <Link href="/auctions/create">Sell my car</Link>
