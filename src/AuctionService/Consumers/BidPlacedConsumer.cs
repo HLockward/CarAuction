51b683cc5494a65a890ef89e37fc155ff25750f4
HLockward <HLockward12@gmail.com>
2025-04-13 20:54:50 -0400

    publish when a bid have been placed and fix the consumers of the auction service

--- src/AuctionService/Consumers/BidPlacedConsumer.cs
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -12,7 +12,7 @@ public class BidPlacedConsumer(AuctionDbContext dbContext) : IConsumer<BidPlaced
     {
         Console.WriteLine("--> Consuming bid placed");
 
-        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
         if (auction.CurrentHighBid is null
             || context.Message.BidStatus.Contains("Accepted")
